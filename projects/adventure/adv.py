099d0ff2e20b500652c04756a45708e841ee0ab9
Heather Nuffer <zzzuzz@gmail.com>
2020-05-13 17:56:02 -0600

    Adds some progress

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -15,8 +15,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 """
@@ -62,67 +62,79 @@ def create_traversal_path():
         0, len(unexplored_directions) - 1)]
     stack.append(move)
     # print("unexplored_directions" + str(unexplored_directions))
+
     while len(visited_rooms) < len(room_graph):
-        # print("length of visited_rooms: " + str(len(visited_rooms)))
-        # while len(stack) > 0:
-        while len(visited_rooms) < len(room_graph):
-            # print("current stack before popping: " + str(stack))
-            move = stack.pop()
-            # Hold on to the prev room's id to update the graph
-            prev_room = player.current_room.id
-            # Travel that direction
-            player.travel(move)
-            # Log that direction
-            traversal_path.append(move)
+        print("num visited rooms: " + str(len(visited_rooms)))
+        # print("current stack before popping: " + str(stack))
+        move = stack.pop()
+        # Hold on to the prev room's id to update the graph
+        prev_room = player.current_room.id
+        # Travel that direction
+        player.travel(move)
+        print("In room " + str(player.current_room.id))
+        # Log that direction
+        traversal_path.append(move)
 
-            # Add current_room to visited_rooms
-            visited_rooms.add(player.current_room)
-            # print("traversal_path: " + str(traversal_path))
-            current_exits = player.current_room.get_exits()
-            # Update the graph
+        # Add current_room to visited_rooms
+        visited_rooms.add(player.current_room)
+        # print("traversal_path: " + str(traversal_path))
+        current_exits = player.current_room.get_exits()
+        # Update the graph
 
-            # Update the entry for the prev room
-            graph[prev_room][move] = player.current_room.id
+        # Update the entry for the prev room
+        graph[prev_room][move] = player.current_room.id
 
-            # Make an entry for the current room if it's not already there
-            if player.current_room.id not in graph:
-                graph[player.current_room.id] = dict()
-                for exit in current_exits:
-                    graph[player.current_room.id][exit] = '?'
+        # Make an entry for the current room if it's not already there
+        if player.current_room.id not in graph:
+            graph[player.current_room.id] = dict()
+            for exit in current_exits:
+                graph[player.current_room.id][exit] = '?'
 
-            # Update the entry for the current room.
-            opposites = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
-            opposite = opposites[move]
-            # print(opposite)
-            graph[player.current_room.id][opposite] = prev_room
-            print("graph so far: " + str(graph))
-            # print(graph)
+        # Update the entry for the current room.
+        opposites = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+        opposite = opposites[move]
+        # print(opposite)
+        graph[player.current_room.id][opposite] = prev_room
+        # print("graph so far: " + str(graph))
 
-            # Find which directions are still unexplored
-            unexplored_directions = [entry[0] for entry in list(
-                graph[player.current_room.id].items()) if entry[1] == '?']
-            # If still has unexplored_directions,
-            # Pick a random unexplored direction from the current room.
-            if len(unexplored_directions) > 0:
-                move = unexplored_directions[random.randint(
-                    0, len(unexplored_directions) - 1)]
-                # Add it to the stack
+        # Find which directions are still unexplored
+        unexplored_directions = [entry[0] for entry in list(
+            graph[player.current_room.id].items()) if entry[1] == '?']
+        # If still has unexplored_directions,
+        # Pick a random unexplored direction from the current room.
+        if len(unexplored_directions) > 0:
+            move = unexplored_directions[random.randint(
+                0, len(unexplored_directions) - 1)]
+            # Add it to the stack
+            stack.append(move)
+        else:
+            '''
+            if move in graph[player.current_room.id]:
                 stack.append(move)
             else:
-                if move in graph[player.current_room.id]:
-                    stack.append(move)
-                else:
-                    choices = []
-                    if 'n' in graph[player.current_room.id] and 'n' != move:
-                        choices.append('n')
-                    if 's' in graph[player.current_room.id] and 's' != move:
-                        choices.append('s')
-                    if 'e' in graph[player.current_room.id] and 'e' != move:
-                        choices.append('e')
-                    if 'w' in graph[player.current_room.id] and 'w' != move:
-                        choices.append('w')
-                    next_move = choices[random.randint(0, len(choices) - 1)]
-                    stack.append(next_move)
+                choices = []
+                if 'n' in graph[player.current_room.id] and 'n' != move:
+                    choices.append('n')
+                if 's' in graph[player.current_room.id] and 's' != move:
+                    choices.append('s')
+                if 'e' in graph[player.current_room.id] and 'e' != move:
+                    choices.append('e')
+                if 'w' in graph[player.current_room.id] and 'w' != move:
+                    choices.append('w')
+                next_move = choices[random.randint(0, len(choices) - 1)]
+                stack.append(next_move)
+            '''
+            choices = []
+            if 'n' in graph[player.current_room.id]:
+                choices.append('n')
+            if 's' in graph[player.current_room.id]:
+                choices.append('s')
+            if 'e' in graph[player.current_room.id]:
+                choices.append('e')
+            if 'w' in graph[player.current_room.id]:
+                choices.append('w')
+            next_move = choices[random.randint(0, len(choices) - 1)]
+            stack.append(next_move)
 
     print("Traversal_path is made! " + str(traversal_path))
     return traversal_path
