e3cc14975d67b657262a58789b85307ae537d960
Heather Nuffer <zzzuzz@gmail.com>
2020-05-13 11:38:21 -0600

    Adds island search code from lecture day 3

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -36,14 +36,15 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+traversal_path = ['n', 'n']
 
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
-
+print("visited_rooms at start: " + str(visited_rooms))
+'''
 # Traversal graph creation
 graph = dict()
 graph[player.current_room.id] = dict()
@@ -53,10 +54,57 @@ for exit in current_exits:
 print("graph so far: ")
 print(graph)
 
+# Find which directions are still unexplored
+unexplored_directions = [entry[0] for entry in list(
+    graph[player.current_room.id].items()) if entry[1] == '?']
+# print("unexplored_directions" + str(unexplored_directions))
+
+while len(unexplored_directions) > 0:
+    # Pick a random unexplored direction from the current room.
+    move = unexplored_directions[random.randint(
+        0, len(unexplored_directions) - 1)]
+    print("time to go " + move)
+
+    # Hold on to the prev room's id to update the graph
+    prev_room = player.current_room.id
+    # Travel that direction
+    player.travel(move)
+    # Log that direction
+    traversal_path.append(move)
+    print("traversal_path: " + str(traversal_path))
+    current_exits = player.current_room.get_exits()
+    # Update the graph
+
+    # Update the entry for the prev room
+    graph[prev_room][move] = player.current_room.id
+
+    # Make an entry for the current room if it's not already there
+    if player.current_room.id not in graph:
+        graph[player.current_room.id] = dict()
+        for exit in current_exits:
+            graph[player.current_room.id][exit] = '?'
+
+    # Update the entry for the current room.
+    opposites = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+    opposite = opposites[move]
+    # print(opposite)
+    graph[player.current_room.id][opposite] = prev_room
+    print("graph so far: ")
+    print(graph)
+
+    # Find which directions are still unexplored
+    unexplored_directions = [entry[0] for entry in list(
+        graph[player.current_room.id].items()) if entry[1] == '?']
+
+# When current room has no unexplored paths, walk back to nearest room that does contain an unexplored path.
+#   -- Use a bfs for a room with a '?' for an exit.
+'''
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
+    print("visited_rooms: " + str(visited_rooms))
 
+print("visited_rooms: ", str(visited_rooms))
 if len(visited_rooms) == len(room_graph):
     print(
         f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
@@ -69,6 +117,8 @@ else:
 # UNCOMMENT TO WALK AROUND
 #######
 player.current_room.print_room_description(player)
+
+'''
 while True:
     cmds = input("-> ").lower().split(" ")
     if cmds[0] in ["n", "s", "e", "w"]:
@@ -77,3 +127,4 @@ while True:
         break
     else:
         print("I did not understand that command.")
+'''
